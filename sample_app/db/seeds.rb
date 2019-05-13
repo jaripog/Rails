6a040324637c3b807823f117b3f21fc99930779e
jaripog <jaripog@outlook.com>
2019-05-13 12:23:07 +0530

    sample User Signup | Login completed

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "JARIPOG",
+    email: "example@jaripog.inc",
+    password:              "qwerty123",
+    password_confirmation: "qwerty123",
+    admin: true)
+
+99.times do |n|
+name  = Faker::Name.name
+email = "example-#{n+1}@jaripog.inc"
+password = "Password-1"
+User.create!(name:  name,
+      email: email,
+      password:              password,
+      password_confirmation: password)
+end
